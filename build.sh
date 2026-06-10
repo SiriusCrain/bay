b6ef8c733815b9b51830f75d854fbf42ff9bae92
Yuriy Matskanyuk <yuriy.matskanyuk@gmail.com>
2026-06-10 13:31:19 +0500

    chore: Rename docker image

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
-docker build -f Dockerfile.build -t amazon-q-builder .
+docker build -f Dockerfile.build -t bay-desktop-builder .
 docker run --rm \
     -v "$PWD:/src" \
-    -v amazon-q-cargo:/root/.cargo/registry \
+    -v bay-desktop-cargo:/root/.cargo/registry \
     -e HOST_UID=$(id -u) -e HOST_GID=$(id -g) \
     --cap-add SYS_ADMIN --device /dev/fuse \
-    amazon-q-builder --skip-tests --skip-lints --variant full "$@"
+    bay-desktop-builder --skip-tests --skip-lints --variant full "$@"
